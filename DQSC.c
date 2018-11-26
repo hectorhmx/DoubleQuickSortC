e09fc3d479451526f164f3dc5b377b00d27097c6
hectorhmx <hectorhmx.55@gmail.com>
2018-11-25 19:26:58 -0600

    Agregadas funciones principales

---
+++ DQSC.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+	int tam;
+	int *elementos;
+}Lista;
+
+void printList(Lista lista){
+        int i;
+	printf("[");
+        for(i=0;i<5;i++){
+                printf("%d,",lista.elementos[i]);
+        }
+        printf("]\n");
+}
+
+
+Lista crearLista(int tam){
+	Lista lista;
+	lista.tam = tam;
+	lista.elementos = (int*)calloc(tam,sizeof(int));
+	return lista;
+}
+
+int aleatorio(int tam){
+	return rand()%(tam);
+}
+
+void swap(Lista *list,int x, int y){
+	int aux = list->elementos[x];
+	list->elementos[x] = list->elementos[y];
+	list->elementos[y] = aux;
+}
+
+void fillList(Lista *list,int caso){
+	int i = 0;
+	if(caso ==0){//peor caso
+		for(i=0;i<list->tam;i++){
+			list->elementos[i] = i;
+		}
+	}
+	else if(caso == 1){
+		for(i=0;i<list->tam;i++){
+			list->elementos[i] = aleatorio(list->tam);
+		}
+	}
+	else if(caso == 2){
+		for(i=0;i<list->tam;i++){
+                	list->elementos[i] = i; //cambiar por aleatorio
+                }
+		//ordenar
+		swap(list,(int) (list->tam-1)/2, list->tam-1);
+	}
+	else{
+		printf("Error en el caso");
+
+	}
+}
+
+
+int main(){
+	Lista lista = crearLista(5);
+	int i;
+	for(i=0;i<5;i++){
+		lista.elementos[i] = i;
+	}
+	fillList(&lista,2);
+	printList(lista);
+	return 0;
+}
